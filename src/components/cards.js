b6c47cff55b971d6157f6bced3139ae273f56fa1
daxas-boop <daxas.swd@hotmail.com>
2020-10-05 23:57:30 -0300

    Adds cards.js Initial array of cards

---
+++ src/components/cards.js
@@ -0,0 +1,77 @@
+import KiritoImg from './../assets/images/Kirito.jpg'
+import HisokaImg from './../assets/images/Hisoka.jpeg'
+import LuffyImg from './../assets/images/Luffy.jpg'
+import ZoroImg from './../assets/images/Zoro.jpg'
+import NeteroImg from './../assets/images/Netero.jpg'
+import KilluaImg from './../assets/images/Killua.jpg'
+import IppoImg from './../assets/images/Ippo.jpg'
+import LelouchImg from './../assets/images/Lelouch.jpg'
+import MirthaImg from './../assets/images/Mirtha.jpg'
+import EdwardImg from './../assets/images/Edward.jpg'
+import TrumpImg from './../assets/images/Trump.jpg'
+import ZeroTwoImg from './../assets/images/ZeroTwo.jpg'
+
+const cards = [
+    {
+        name:'Kirito',
+        anime:'Sword Art Online',
+        src:KiritoImg
+    },
+    {
+        name:'Zero Two',
+        anime:'Darling in the Franxx',
+        src:ZeroTwoImg
+    },
+    {
+        name:'Hisoka Morow',
+        anime:'Hunter x Hunter',
+        src:HisokaImg
+    },
+    {
+        name:'Monkey D. Luffy',
+        anime:'One Piece',
+        src:LuffyImg
+    },
+    {
+        name:'Roronoa Zoro',
+        anime:'One Piece',
+        src:ZoroImg
+    },
+    {
+        name:'Isaac Netero',
+        anime:'Hunter x Hunter',
+        src:NeteroImg
+    },
+    {
+        name:'Killua Zoldyck',
+        anime:'Hunter x Hunter',
+        src:KilluaImg
+    },
+    {
+        name:'Makunouchi Ippo',
+        anime:'Hajime no Ippo',
+        src:IppoImg
+    },
+    {
+        name:'Lelouch vi Britannia',
+        anime:'Code Geass',
+        src:LelouchImg
+    },
+    {
+        name:'Mirtha Legrand',
+        anime:'Error not found',
+        src:MirthaImg
+    },
+    {
+        name:'Edward Elric',
+        anime:'Fullmetal Alchemist',
+        src:EdwardImg
+    },
+    {
+        name:'Donald Trump',
+        anime:'USA ?',
+        src:TrumpImg
+    },
+]
+
+export default cards;
