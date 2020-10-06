37254ef3285b35f4a83844d31ab1bb3a8ae33b2e
daxas-boop <daxas.swd@hotmail.com>
2020-10-06 00:03:30 -0300

    Updates

---
+++ precache-manifest.122267fea0ebfc83f9c582466379cca9.js
@@ -0,0 +1,70 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "99ea8320148cbc18777e89acdcb2eca7",
+    "url": "/memory-card/index.html"
+  },
+  {
+    "revision": "095393334be5b036cc4e",
+    "url": "/memory-card/static/js/2.c4708710.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/memory-card/static/js/2.c4708710.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4e1ceab42dd62656dca4",
+    "url": "/memory-card/static/js/main.3055aa27.chunk.js"
+  },
+  {
+    "revision": "2d10a56d9bedc8f13787",
+    "url": "/memory-card/static/js/runtime-main.4cf988ce.js"
+  },
+  {
+    "revision": "4042aa957fcab42396649592a12bf44b",
+    "url": "/memory-card/static/media/Edward.4042aa95.jpg"
+  },
+  {
+    "revision": "3b321f010881b4c2cafa7783fe56ecd1",
+    "url": "/memory-card/static/media/Hisoka.3b321f01.jpeg"
+  },
+  {
+    "revision": "357908ba28dc049de7b381bfb446c923",
+    "url": "/memory-card/static/media/Ippo.357908ba.jpg"
+  },
+  {
+    "revision": "1bc1031d62ae880f52da50d28027c593",
+    "url": "/memory-card/static/media/Killua.1bc1031d.jpg"
+  },
+  {
+    "revision": "bcbd030d8504574eee9ec7f861986af4",
+    "url": "/memory-card/static/media/Kirito.bcbd030d.jpg"
+  },
+  {
+    "revision": "9ca8e2b6006d74e122e2149f7a929b1c",
+    "url": "/memory-card/static/media/Lelouch.9ca8e2b6.jpg"
+  },
+  {
+    "revision": "58a63fac5b3a3eb66c6372b3eda31b13",
+    "url": "/memory-card/static/media/Luffy.58a63fac.jpg"
+  },
+  {
+    "revision": "e731e04f2b8bbfef217b2ba76511eeb5",
+    "url": "/memory-card/static/media/Mirtha.e731e04f.jpg"
+  },
+  {
+    "revision": "3b1bb249319d6127d8448825c830ffdd",
+    "url": "/memory-card/static/media/Netero.3b1bb249.jpg"
+  },
+  {
+    "revision": "e96c186ad1c8b2684d9ffcba1c0e33f5",
+    "url": "/memory-card/static/media/Trump.e96c186a.jpg"
+  },
+  {
+    "revision": "d684bc0521e97a46bf6cf44e1c3e4fe5",
+    "url": "/memory-card/static/media/ZeroTwo.d684bc05.jpg"
+  },
+  {
+    "revision": "54dffcab4ff325df3fe49f3d947666b1",
+    "url": "/memory-card/static/media/Zoro.54dffcab.jpg"
+  }
+]);
